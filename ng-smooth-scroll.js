998fea6e0112b349445691f400ef92a8727a5bed
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-08-25 22:52:05 -0500

    simple jquery solution complete

--- ng-smooth-scroll.js
+++ ng-smooth-scroll.js
@@ -7,19 +7,16 @@
             link: function (scope, element) {
                 console.log('[smoothScrol] init');
 
-                var trigger=angular.element('.scroll-to', element);
+                var trigger = angular.element('.scroll-to', element);
 
                 trigger.on('click', function () {
-                    var $this = angular.element(this);
-                    target = $(this).attr('href');
+                    var $this = angular.element(this),
+                        targetHref = $this.attr('href'),
+                        targetLocation = $("a[name=" + targetHref.slice(1) + "]");
 
-                    console.log(name);
-                    console.log(target.slice(1));
-                    console.log($("[name=one"));
-
-                    // $('html, body').animate({
-                    //     scrollTop: target.offset().top
-                    // }, 1000);
+                    $('html, body').animate({
+                        scrollTop: targetLocation.offset().top
+                    }, 800);
 
                     return false;
                 });
